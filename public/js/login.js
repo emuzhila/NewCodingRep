d5446aa629ef60b3fe7775830811ef9f683be4d1
SuperninjaX2 <80552583+SuperninjaX2@users.noreply.github.com>
2023-12-21 11:37:54 +0200

    added authentication

---
+++ public/js/login.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector('button')
+let error = document.querySelector('.error')
+let password = 'test';
+let user='test'
+function check() {
+	let pass = document.querySelector('#password');
+let name = document.querySelector('#username');
+    console.log('Function called');
+    if (pass.value == password&& name.value == user) {
+    	window.location.href='index.html'
+        console.log('correct password');
+    } else {
+        error.innerHTML='incorrect password and username';
+    }
+    
+}
+btn.addEventListener('click', check)
