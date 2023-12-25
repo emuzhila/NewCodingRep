cc86db713409f098f6879d12c84d758649b0e454
SuperninjaX2 <80552583+SuperninjaX2@users.noreply.github.com>
2023-12-25 21:18:04 +0200

    new visuals

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,16 @@
 let btn = document.querySelector('button')
 let error = document.querySelector('.error')
+
 let password = 'test';
 let user='test'
+
+
 function check() {
 	let pass = document.querySelector('#password');
-let name = document.querySelector('#username');
+  let name = document.querySelector('#username');
+    
     console.log('Function called');
+    
     if (pass.value == password&& name.value == user) {
     	window.location.href='index.html'
         console.log('correct password');
