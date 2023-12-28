1f13ed6f5fcccdabd1aef07eb50ca9b8b95a0286
SuperninjaX2 <80552583+SuperninjaX2@users.noreply.github.com>
2023-12-28 06:41:40 +0200

    Add files via upload

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,78 +1,91 @@
-
-
-  // Graphs
-  var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
+// Graphs
+var ctx = document.getElementById('myChart');
+// eslint-disable-next-line no-unused-vars
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ],
+    datasets: [{
+      data: [
+        15339,
+        21345,
+        18483,
+        24003,
+        23489,
+        24092,
+        12034
       ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
+      lineTension: 0,
+      backgroundColor: 'transparent',
+      borderColor: '#007bff',
+      borderWidth: 4,
+      pointBackgroundColor: '#007bff'
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
       }]
     },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
+    legend: {
+      display: false
     }
-  })
+  }
+});
 
-  let box = document.querySelector(".box");
- async ()=>{
+let box = document.querySelector(".box");
+
+(async () => {
   try {
     let response = await fetch("/getdata");
-    
+
     if (!response.ok) {
       console.error(`Server returned an error: ${response.status}`);
-      // Optionally, you can log the response text for more details
       console.error(await response.text());
       return;
     }
 
     let data = await response.json();
-    // Update your box or handle the data as needed
-    console.log("${data}")
-    box.innerHTML = ` <tr>
-                  <td>${data.RoomNumber
-                  }</td>
-                  <td>${data.RoomType}</td>
-                  <td>${data.Price}</td>
-                  
-                </tr>
-                <tr>
-                  `;
+
+    // Log the actual structure of the JSON response
+    console.log("JSON response:", data);
+
+    // Check if the response is an array with at least one object
+    if (Array.isArray(data) && data.length > 0) {
+      // Create a new row in the table for each object in the JSON response
+      let rows = data.map((roomData) => {
+        // Convert the JSON data to a string
+        let jsonString = JSON.stringify(roomData, null, 2);
+
+        // Create a row for the current object
+        return `
+          <tr>
+            <td>${roomData.RoomNumber}</td>
+            <td>${roomData.RoomType}</td>
+            <td>${roomData.Price}</td>
+          
+          </tr>`;
+      });
+
+      // Update your box or handle the data as needed
+      box.innerHTML = rows.join('');
+    } else {
+      console.error("Invalid data structure in the JSON response");
+    }
   } catch (error) {
     console.error("An unexpected error occurred:", error);
   }
-};
+})();
 
 
